d0cebcaf0ddacf6fecf57e4c9ae4ee718bf544ca
AvaNoilhetas <a.noilhetas@hotmail.fr>
2021-01-19 02:36:32 +0100

    [+]: Add paper effect

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,12 @@
 import React from "react";
 import banner from "./../assets/img/banner.jpg";
+import paper from "./../assets/img/paper-effect.svg";
 
 const Banner = () => {
   return (
     <section className="relative h-96 overflow-hidden mb-4">
       <img src={banner} alt="Vinted" className="-mt-20" />
+      <img src={paper} alt="Vinted" className="absolute -bottom-0.5 left-1/2" />
       <div className="absolute inset-0 flex items-center">
         <div className="bg-white rounded p-8 ml-8 max-w-sm">
           <h1 className="title-1 pb-8">
